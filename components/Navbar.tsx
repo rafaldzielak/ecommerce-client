9589029efeced9faf926ac4f25ab74eb10830796
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-07-16 10:16:13 +0200

    Navbar UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import getCategories from "@/actions/getCategories";
+import Link from "next/link";
+import React, { FC } from "react";
+import MainNav from "./MainNav";
+import Container from "./ui/Container";
+import NavbarActions from "./NavbarActions";
 
-const Navbar = () => {
-  return <div className='border-b'>Navbar</div>;
+const Navbar: FC = async () => {
+  const categories = await getCategories();
+
+  return (
+    <Container>
+      <div className='border-b'>
+        <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center'>
+          <Link href='/' className='ml-4 flex lg:ml-0 gap-x-2'>
+            <p className='font-bold text-xl'>STORE</p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Navbar;
---
+++ components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import React, { ButtonHTMLAttributes, FC, forwardRef } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, disabled, type = "button", ...props }, ref) => {
+  return (
+    <button
+      className={cn(
+        "w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition",
+        className
+      )}
+      ref={ref}>
+      {children}
+    </button>
+  );
+});
+
+Button.displayName = "Button";
+
+export default Button;
