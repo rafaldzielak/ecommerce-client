74e035a97fdcbdc994b29e3b8bab70506729696a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-07-16 11:40:48 +0200

    Billboards UI

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,20 @@
+import getBillboards from "@/actions/getBillboards";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
 import React from "react";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboards("9e874d74-62c1-4cc3-9aa7-ced96c62e525");
+
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard billboard={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
